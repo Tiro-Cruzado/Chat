4de274e5964b3933a32f62dd61a9258d57ee365b
Roberto <robertomelo287@gmail.com>
2023-05-04 22:45:17 -0300

    Feat/Front/Modal: remove of comment of file index.html and add componnet in App

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,19 @@
-import { Modal } from './components/Modal'
+import { BottonSheet } from './components/BottonSheet'
+// import { Card } from './components/Card'
+// import { Modal } from './components/Modal'
 
 function App() {
   return (
     <>
-      <Modal />
+      {/* <Modal>
+        <Card>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat
+          tenetur minus rerum est atque voluptates amet asperiores mollitia,
+          inventore expedita eius? Ea odio sed fugiat aliquid repellendus at
+          quas iusto?
+        </Card>
+      </Modal> */}
+      <BottonSheet />
     </>
   )
 }
