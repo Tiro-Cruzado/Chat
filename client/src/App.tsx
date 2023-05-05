4b52dae81e40c11b9b6a4f7fd3f23c69ae4106be
sarahmelo <sarahcbdg@gmail.com>
2023-05-05 20:03:44 -0300

    chore: clean code

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,10 @@
 import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './themes/default'
-import { Text } from './components/Text/style'
 
 function App() {
   return (
     <>
-      <ThemeProvider theme={defaultTheme}>
-        <Text fontSize="large" color="dark-gray" as={'h1'}>
-          Olá mundo
-        </Text>
-      </ThemeProvider>
+      <ThemeProvider theme={defaultTheme}></ThemeProvider>
     </>
   )
 }
