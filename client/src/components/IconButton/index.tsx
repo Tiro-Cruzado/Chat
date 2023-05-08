4ba7aaf4e3b3a1a13d6a39f7cc8a842d6b9e8f1b
Christian Valentim <110266894+Chris-Valentim@users.noreply.github.com>
2023-05-08 20:50:20 -0300

    Front/Feat/IconButton: Criação do componente IconButton (#3)

---
+++ client/src/components/IconButton/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import * as S from './style'
+import { PaperPlaneTilt } from '@phosphor-icons/react'
+
+type iconButtonProps = {
+  disabled?: boolean
+  onClick: () => void
+}
+
+const IconButton = ({ disabled, onClick }: iconButtonProps) => {
+  return (
+    <S.Container disabled={disabled} onClick={onClick}>
+      <PaperPlaneTilt size={20} color="#ffffff" weight="bold" />
+    </S.Container>
+  )
+}
+
+export default IconButton
