b7bc66371a48cb2e35f4251012105d157ae3645a
Richard Valentim <ric_val97@hotmail.com>
2023-05-25 22:39:30 -0300

    isolating chat creation

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,8 @@
 import dotenv from "dotenv";
 import http from "node:http";
-import { Server } from "socket.io";
 
 import createApp from "./interfaces/express";
-import { ChatSocket } from "./interfaces/socketio/ChatSocket";
-import { ChatRoomRepository } from "./infrasctructure/repositories/implementations/ChatRoomRepository";
+import { createChatSocket } from "./interfaces/socketio";
 
 dotenv.config();
 
@@ -12,8 +10,7 @@ const APP_PORT = process.env.APP_PORT || 3000;
 
 const app = createApp();
 const httpServer = http.createServer(app);
-const io = new Server(httpServer);
-const chatSocket = new ChatSocket(io, ChatRoomRepository.getInstance())
+const chatSocket = createChatSocket(httpServer);
 
 chatSocket.init()
 
