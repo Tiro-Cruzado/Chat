a239cd88e117dcd4e3cab97b6de8acc1b3344d04
Roberto.Melo <robertomelo287@gmail.com>
2023-06-27 01:04:22 -0300

    Front/Feat/BigAvatar: start develop component

--- client/src/components/BigAvatar/index.tsx
+++ client/src/components/BigAvatar/index.tsx
@@ -1,7 +1,38 @@
+import { lorelei } from '@dicebear/collection'
+import { createAvatar } from '@dicebear/core'
+import { useMemo } from 'react'
 import * as S from './styles'
+import { Info } from '@phosphor-icons/react'
 
-const BigAvatar = () => {
-  return <S.WrapperFront>Olá mundo</S.WrapperFront>
+type BigAvatarProps = {
+  src?: string
+}
+
+const BigAvatar = ({ src }: BigAvatarProps) => {
+  src = useMemo(() => {
+    return createAvatar(lorelei, {
+      size: 110,
+    }).toDataUriSync()
+  }, [])
+
+  return (
+    <>
+      <S.WrapperFront>
+        <img src={src} alt="opa" />
+        <Info
+          size={26}
+          weight="bold"
+          style={{
+            position: 'absolute',
+            zIndex: '10',
+            bottom: '20px',
+            right: '8px',
+          }}
+        />
+      </S.WrapperFront>
+      <S.WrapperBack>Algumas informações virão aqui</S.WrapperBack>
+    </>
+  )
 }
 
 export { BigAvatar }
--- client/src/components/BigAvatar/styles.ts
+++ client/src/components/BigAvatar/styles.ts
@@ -4,8 +4,52 @@ const WrapperFront = styled.div`
   ${({ theme }) => css`
     height: 160px;
     width: 160px;
+
     background-color: ${theme.colors.cyane};
+    border-radius: 32px 32px 0px 32px;
+
+    margin-top: 120px;
+    position: relative;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    :hover {
+      transition: 0.3;
+      transform: rotateY(0.5turn);
+      transform-style: preserve-3d;
+    }
+
+    ::before {
+      content: '';
+      background-color: transparent;
+      position: absolute;
+      border: 3px solid currentColor;
+      transform: var(--level-one);
+    }
   `}
 `
 
-export { WrapperFront }
+const WrapperBack = styled.div`
+  ${({ theme }) => css`
+    height: 160px;
+    width: 160px;
+
+    background-color: ${theme.colors.cyane};
+    border-radius: 32px 32px 0px 32px;
+
+    margin-top: 20px;
+    display: flex;
+    text-align: center;
+    align-items: center;
+    z-index: 11;
+    margin-top: -160px;
+
+    :hover {
+      transition: 0.3;
+      transform: rotateY(0.5turn);
+    }
+  `}
+`
+
+export { WrapperFront, WrapperBack }
