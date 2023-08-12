9340236e45f69064d0229fd8a7fe3a8d8f1eaacf
Richard Valentim <ric_val97@hotmail.com>
2023-08-12 15:56:17 -0300

    Configuring tsc and eslint; Fix lint and types;

--- server/src/interfaces/socketio/index.ts
+++ server/src/interfaces/socketio/index.ts
@@ -1,9 +1,10 @@
 import { Server } from "socket.io";
+import http from "node:http";
 
 import { ChatSocket } from "../../interfaces/socketio/ChatSocket";
 import { ChatRoomRepository } from "../../infrasctructure/repositories/implementations/ChatRoomRepository";
 
-export const createChatSocket = (httpServer: any): ChatSocket => {
+export const createChatSocket = (httpServer: http.Server): ChatSocket => {
   const io = new Server(httpServer);
   return new ChatSocket(io, ChatRoomRepository.getInstance())
 }
